8a62cc9b8a46e8d3f04e63d6e930ccac831774e1
Tcheneghon Motcheyo <63323044+Herman-Motcheyo@users.noreply.github.com>
2020-10-29 20:51:04 +0100

    Update settings.py

--- hermanApp/hermanApp/settings.py
+++ hermanApp/hermanApp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=1rg&d#72sdk+&!z4iup=vrj4o@!98ia0um2q_9v(g+m8czxw+'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
